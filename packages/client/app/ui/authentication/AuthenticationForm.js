f22cbf5b622b9eec030c6d4667b3de9d5b539311
grgml <grigormalo@gmail.com>
2022-10-18 15:08:39 +0200

    fix(ui): replace anchor tags with Link element

--- packages/client/app/ui/authentication/AuthenticationForm.js
+++ packages/client/app/ui/authentication/AuthenticationForm.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-
+import { Link } from 'react-router-dom'
 import { grid } from '@coko/client'
 
 import { Form, Button, Link } from '../common'
