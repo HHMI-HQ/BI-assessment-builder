9d8e0b89361f3df4742ce7547c096ae052e7df21
grgml <grigormalo@gmail.com>
2022-10-21 10:57:49 +0200

    feat(server): add permissions for gql endpoints

--- packages/server/config/permissions.js
+++ packages/server/config/permissions.js
@@ -1,15 +1,62 @@
-const { rule } = require('@coko/server/authorization')
+const { rule, isAuthenticated } = require('@coko/server/authorization')
+
+const isEditor = rule()(async (_, __, ctx) => {
+  const UserModel = ctx.connectors.User.model
+  const user = await UserModel.query().findById(ctx.user)
+  return user.hasGlobalRole('editor')
+})
+
+const isAdmin = rule()(async (_, __, ctx) => {
+  const UserModel = ctx.connectors.User.model
+  const user = await UserModel.query().findById(ctx.user)
+  return user.hasGlobalRole('admin')
+})
 
 const permissions = {
   Mutation: {
-    deleteUsers: rule()(async (_, args, ctx) => {
-      // allow only if current user is not in the list of ids to delete
-      return args.ids.indexOf(ctx.user) === -1
-    }),
-    deactivateUsers: rule()(async (_, args, ctx) => {
-      // allow only if current user is not in the list of ids to deactivate
-      return args.ids.indexOf(ctx.user) === -1
-    }),
+    // Users
+    submitQuestionnaire: isAuthenticated,
+    updateUserProfile: isAuthenticated,
+    updatePassword:
+      isAuthenticated &&
+      rule()(async (_, { input: { id } }, { user }) => {
+        // only if id from arguments is same as current user's id
+        return id === user
+      }),
+    deleteUsers:
+      isAuthenticated &&
+      isAdmin &&
+      rule()(async (_, args, ctx) => {
+        // allow only if current user is not in the list of ids to delete
+        return args.ids.indexOf(ctx.user) === -1
+      }),
+    deactivateUsers:
+      isAuthenticated &&
+      isAdmin &&
+      rule()(async (_, args, ctx) => {
+        // allow only if current user is not in the list of ids to deactivate
+        return args.ids.indexOf(ctx.user) === -1
+      }),
+
+    // Teams
+    updateGlobalTeams: isAuthenticated && isAdmin,
+
+    // Questions
+    createQuestion: isAuthenticated,
+    updateQuestion: isAuthenticated,
+    submitQuestion: isAuthenticated,
+    rejectQuestion: isAuthenticated && isEditor,
+    moveQuestionVersionToReview: isAuthenticated && isEditor,
+    publishQuestionVersion: isAuthenticated && isEditor,
+  },
+  Query: {
+    // Users
+    filterUsers: isAuthenticated && isAdmin,
+    // Teams
+    getNonTeamMemberUsers: isAuthenticated && isAdmin,
+    // Questions:
+    getAuthorDashboard: isAuthenticated,
+    getManagingEditorDashboard: isAuthenticated && isEditor,
   },
 }
 
