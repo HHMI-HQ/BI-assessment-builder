08630e83a0b2eeca76b1d0b89cbc50b71192c098
grgml <grigormalo@gmail.com>
2022-10-28 17:19:36 +0200

    feat(server): check if user is active; detailed check for updating question

--- packages/server/config/permissions.js
+++ packages/server/config/permissions.js
@@ -1,16 +1,104 @@
 const { rule, isAuthenticated } = require('@coko/server/authorization')
 
+const isActive = rule()(async (_, __, ctx) => {
+  if (ctx.user) {
+    const UserModel = ctx.connectors.User.model
+    const user = await UserModel.query().findById(ctx.user)
+    return user.isActive
+  }
+
+  return false
+})
+
 const isEditor = rule()(async (_, __, ctx) => {
+  if (!ctx.user) return false
+
   const UserModel = ctx.connectors.User.model
   const user = await UserModel.query().findById(ctx.user)
-  return user.hasGlobalRole('editor')
+  return user.isActive && user.hasGlobalRole('editor')
 })
 
 const isAdmin = rule()(async (_, __, ctx) => {
+  if (!ctx.user) return false
+
   const UserModel = ctx.connectors.User.model
   const user = await UserModel.query().findById(ctx.user)
-  return user.hasGlobalRole('admin')
+  return user.isActive && user.hasGlobalRole('admin')
 })
+
+const isAuthor = rule()(async (_, { questionId }, ctx) => {
+  if (!ctx.user) return false
+
+  const UserModel = ctx.connectors.User.model
+  const user = await UserModel.query().findById(ctx.user)
+
+  if (!user.isActive) return false
+
+  return isQuestionAuthor(ctx.connectors.Team.model, ctx.user, questionId)
+})
+
+const isQuestionAuthor = async (teamModel, user, questionId) => {
+  const questionAuthor = await teamModel
+    .query()
+    .leftJoin('team_members', 'team_members.team_id', 'teams.id')
+    .select('teams.role')
+    .findOne({
+      'teams.object_id': questionId,
+      'team_members.user_id': user,
+      'teams.role': 'author',
+    })
+
+  return !!questionAuthor
+}
+
+const canUpdateQuestion = rule()(
+  async (_, { questionId, questionVersionId }, ctx) => {
+    // must be logged in
+    if (!ctx.user) return false
+
+    const UserModel = ctx.connectors.User.model
+    const user = await UserModel.query().findById(ctx.user)
+    // must be active
+    if (!user.isActive) return false
+
+    const QuestionModel = ctx.connectors.Question.model
+
+    const question = await QuestionModel.query()
+      .leftJoin(
+        'question_versions',
+        'questions.id',
+        'question_versions.question_id',
+      )
+      .select(
+        'questions.id',
+        'questions.rejected',
+        'question_versions.submitted',
+        'question_versions.under_review',
+        'question_versions.in_production',
+        'question_versions.published',
+      )
+      .findOne({
+        'questions.id': questionId,
+        'question_versions.id': questionVersionId,
+      })
+
+    if (question.rejected) return false
+
+    if (!question.submitted) {
+      // needs to be the author
+      return isQuestionAuthor(ctx.connectors.Team.model, ctx.user, questionId)
+    }
+
+    // the only other editable state === 'inProduction'
+    if (question.inProduction) {
+      // only editors can edit
+      return user.hasGlobalRole('editor')
+    }
+
+    // if submitted, under review or published
+    return false
+  },
+)
 
 const permissions = {
   Mutation: {
@@ -18,22 +106,20 @@ const permissions = {
     resendVerificationEmailAfterLogin: isAuthenticated,
     // Users
     submitQuestionnaire: isAuthenticated,
-    updateUserProfile: isAuthenticated,
+    updateUserProfile: isActive,
     updatePassword:
-      isAuthenticated &&
+      isActive &&
       rule()(async (_, { input: { id } }, { user }) => {
         // only if id from arguments is same as current user's id
         return id === user
       }),
     deleteUsers:
-      isAuthenticated &&
       isAdmin &&
       rule()(async (_, args, ctx) => {
         // allow only if current user is not in the list of ids to delete
         return args.ids.indexOf(ctx.user) === -1
       }),
     deactivateUsers:
-      isAuthenticated &&
       isAdmin &&
       rule()(async (_, args, ctx) => {
         // allow only if current user is not in the list of ids to deactivate
@@ -41,27 +127,28 @@ const permissions = {
       }),
 
     // Teams
-    addTeamMember: isAuthenticated,
-    updateTeamMembership: isAuthenticated,
-    updateGlobalTeams: isAuthenticated && isAdmin,
+    addTeamMember: isActive,
+    updateTeamMembership: isActive,
+    updateGlobalTeams: isAdmin,
 
     // Questions
-    createQuestion: isAuthenticated,
-    updateQuestion: isAuthenticated,
-    submitQuestion: isAuthenticated,
-    rejectQuestion: isAuthenticated && isEditor,
-    moveQuestionVersionToReview: isAuthenticated && isEditor,
-    publishQuestionVersion: isAuthenticated && isEditor,
+    createQuestion: isActive,
+    updateQuestion: canUpdateQuestion,
+    submitQuestion: isAuthor,
+    rejectQuestion: isEditor,
+    moveQuestionVersionToReview: isEditor,
+    moveQuestionVersionToProduction: isEditor,
+    publishQuestionVersion: isEditor,
   },
   Query: {
     // Users
-    filterUsers: isAuthenticated && isAdmin,
+    filterUsers: isAdmin,
     // Teams
-    getGlobalTeams: isAuthenticated && isAdmin,
-    getNonTeamMemberUsers: isAuthenticated && isAdmin,
+    getGlobalTeams: isAdmin,
+    getNonTeamMemberUsers: isAdmin,
     // Questions:
-    getAuthorDashboard: isAuthenticated,
-    getManagingEditorDashboard: isAuthenticated && isEditor,
+    getAuthorDashboard: isActive,
+    getManagingEditorDashboard: isEditor,
   },
 }
 
