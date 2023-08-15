fc11ce2f1b1e506f9a10555e529472cddac04945
Vignesh <alphavignesh98@gmail.com>
2023-08-15 13:17:06 +0000

    upgrade Cypress to v12.17.2

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,4 @@
+// /* eslint-disable cypress/unsafe-to-chain-command */
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
