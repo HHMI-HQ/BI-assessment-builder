5d25782cb300a254ada868958c1933a3aba4dd8d
Grigor Malo <grigormalo@gmail.com>
2023-08-18 10:32:12 +0000

    Hot fix sso login

--- packages/server/controllers/config.controllers.js
+++ packages/server/controllers/config.controllers.js
@@ -2,7 +2,8 @@ const loginConfigController = () => {
   return {
     showEmailLogin: process.env.CLIENT_SHOW_EMAIL_LOGIN_OPTION === 'true',
     biointeractiveOathClientId: process.env.BIOINTERACTIVE_OAUTH_CLIENT_ID,
-    biointeractiveOathRedirectUri: process.env.BIOINTERACTIVE_OAUTH_CLIENT_ID,
+    biointeractiveOathRedirectUri:
+      process.env.BIOINTERACTIVE_OAUTH_REDIRECT_URI,
   }
 }
 
