3802ea6d59e8587b3096f16f15d32a81e93f7018
Mathias Romeo <hmromeo1991@gmail.com>
2023-09-08 00:44:23 -0300

    fix(*): restored headings component to previous state. reimplemented styles

--- packages/client/app/ui/common/Headings.js
+++ packages/client/app/ui/common/Headings.js
@@ -2,42 +2,19 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled, { css } from 'styled-components'
 
 import { Typography } from 'antd'
 
 const { Title } = Typography
 
-const TitleWrapper = styled.div`
-  align-items: center;
-  background: ${p =>
-    p.level === 1 && `linear-gradient(60deg, #00763a, #8ac341)`};
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  margin: 0;
-  ${p =>
-    p.level === 1 &&
-    css`
-      height: 10rem;
-      & h1 {
-        color: #fff;
-        margin: 0;
-        text-shadow: 0 1px 5px #0006;
-      }
-    `};
-
-  text-align: center;
-  width: 100%;
-`
-
 const Heading = props => {
   const { className, children, level } = props
 
   return (
-    <TitleWrapper className={className} level={level}>
-      <Title>{children}</Title>
-    </TitleWrapper>
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <Title className={className} level={level}>
+      {children}
+    </Title>
   )
 }
 
