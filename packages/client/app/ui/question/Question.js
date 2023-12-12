1239b87fc4e529921558d1f0c1801756fd05fe6e
Grigor Malo <grigormalo@gmail.com>
2023-12-12 16:16:36 +0000

    Fix Item preview

--- packages/client/app/ui/question/Question.js
+++ packages/client/app/ui/question/Question.js
@@ -1387,11 +1387,6 @@ const Question = props => {
 
   const RightArea = (
     <RightAreaWrapper id="question-actions" tabIndex="-1">
-      {showPreviewButton && (
-        <Button onClick={() => setPreview(prev => !prev)}>
-          {!preview ? 'Preview' : 'Continue editing'}
-        </Button>
-      )}
       {readOnly ? null : (
         <AutoSaving
           autoSaving={autoSaving}
