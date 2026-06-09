b8ee0265635ca689e595befe8d00440f07c0a2bb
grgml <grigormalo@gmail.com>
2026-06-09 11:45:54 +0200

    refactor(ui): put reviewer form ui components and form options in a single folder and import from there

--- packages/client/app/ui/review/ReviewerSubmitButton.js
+++ packages/client/app/ui/review/ReviewerSubmitButton.js
@@ -4,6 +4,18 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { grid } from '@coko/client'
 import { Button, Form, Modal } from '../common'
+import {
+  yesOrNoOptions,
+  difficultyOptions,
+  hasIssuesOptions,
+  issuesOptions,
+  questionTypesOptions,
+  distractorsOptions,
+  feedbackEvaluationOptions,
+  feedbackIssuesOptions,
+  concernsOptions,
+  concernsSpecificsOptions,
+} from './reviewerFormUI'
 
 const Label = styled.div`
   font-weight: bold;
@@ -13,181 +25,6 @@ const Label = styled.div`
 const Answer = styled.div`
   margin-block-start: ${grid(1)};
 `
-
-const yesOrNoOptions = [
-  {
-    label: 'Yes',
-    value: true,
-  },
-  {
-    label: 'No',
-    value: false,
-  },
-]
-
-const difficultyOptions = [
-  {
-    label: 'Appropriate',
-    value: 'appropriate',
-  },
-  {
-    label: 'Too difficult',
-    value: 'difficult',
-  },
-  {
-    label: 'Too simple',
-    value: 'simple',
-  },
-]
-
-const hasIssuesOptions = [
-  {
-    label: 'There are NO content-related issues',
-    value: false,
-  },
-  {
-    label: 'There are content-related issues  ',
-    value: true,
-  },
-]
-
-const issuesOptions = [
-  {
-    label: 'There is a factual error in the stem',
-    value: 'factualError',
-  },
-  {
-    label: 'Sufficient detail to answer the item is lacking',
-    value: 'insufficientDetail',
-  },
-  {
-    label:
-      'The content of this item does not incorporate the most up-to-date understanding in the field',
-    value: 'outdated',
-  },
-  {
-    label: 'There are other content-related issues',
-    value: 'simple',
-  },
-]
-
-const questionTypesOptions = [
-  {
-    value: 'essay',
-    label: 'Essay',
-  },
-  {
-    value: 'matching',
-    label: 'Matching',
-  },
-  {
-    value: 'multipleChoiceSingleCorrect',
-    label: 'Multiple Choice',
-  },
-  {
-    value: 'multipleChoice',
-    label: 'Multiple Answers',
-  },
-  {
-    value: 'trueFalse',
-    label: 'Multiple True / False',
-  },
-  {
-    value: 'numerical',
-    label: 'Numerical Answer',
-  },
-  {
-    value: 'trueFalseSingleCorrect',
-    label: 'True / False',
-  },
-
-  {
-    value: 'fillInTheBlank',
-    label: 'Fill-in-the-blank',
-  },
-  {
-    value: 'multipleDropdowns',
-    label: 'Multiple Dropdowns',
-  },
-]
-
-const distractorsOptions = [
-  {
-    value: 'appropriate',
-    label: 'The distractors are appropriate',
-  },
-  {
-    value: 'multipleDistractors',
-    label:
-      'There are multiple correct answers, or it is not clear which answer is correct',
-  },
-  {
-    value: 'externalKnowledge',
-    label:
-      'Knowledge outside the assessed LO is needed to eliminate distractors',
-  },
-  {
-    value: 'misconceptions',
-    label: 'Not all distractors address common misconceptions of the content',
-  },
-]
-
-const feedbackEvaluationOptions = [
-  {
-    label: 'There are NO issues related to the feedback',
-    value: 'noIssues',
-  },
-  {
-    label: 'There are issues with the feedback',
-    value: 'hasIssues',
-  },
-]
-
-const feedbackIssuesOptions = [
-  {
-    value: 'factualErrors',
-    label: 'The feedback contains one or more factual errors',
-  },
-  {
-    value: 'detailsLacking',
-    label: 'The feedback lacks details or is not fully explanatory',
-  },
-  {
-    value: 'hintsToCorrectAnswer',
-    label:
-      'Incorrect response feedback includes information for the correct answer, thereby limiting multiple attempts at the same item',
-  },
-  {
-    value: 'other',
-    label: 'There are other feedback-related issues',
-  },
-]
-
-const concernsOptions = [
-  {
-    label: 'No clarity, grammatical, structural, or other concerns',
-    value: false,
-  },
-  {
-    label: 'Clarity, grammatical, structural, or other concerns',
-    value: true,
-  },
-]
-
-const concernsSpecificsOptions = [
-  {
-    label: 'Clarity concerns',
-    value: 'clarity',
-  },
-  {
-    label: 'Grammatical or structural concerns',
-    value: 'gramatical',
-  },
-  {
-    label: 'Other Item construction concerns',
-    value: 'other',
-  },
-]
 
 const renderAnswer = (options, answer) => {
   return options.find(o => o.value === answer)?.label
